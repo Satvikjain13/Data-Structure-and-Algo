2eb6134632562086f360e10b9bfd5939f3523194
Satvikjain13 <satvikjain0110@gmail.com>
2025-09-12 10:34:05 +0530

    Leetcode Problem

---
+++ 125. Valid Palindrome.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder cleaned =new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(Character.isLetterOrDigit(ch)){
+                cleaned.append(Character.toLowerCase(ch));
+            }
+        }
+        String str=cleaned.toString();
+
+        String reversed=cleaned.reverse().toString();
+
+        return str.equals(reversed);
+    }
+}
